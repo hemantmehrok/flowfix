d0d730a8f92e851dfbe55d2d09395bfc4c900797
hemantmehrok <hemantsinghmehrok@gmail.com>
2025-05-23 19:48:04 -0400

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     fs: {
       allow: ['.', './node_modules/']
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true
   }
 });
